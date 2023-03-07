6e3c01cf9906b8148763887b5ee2c910a3288681
Diego <diegopablo012@gmail.com>
2023-03-06 19:16:00 -0600

    Main creada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        UI ui = new UI();
+        Automata automata = new Automata();
+        String line = ui.read("Ingrese la linea");
+        String LineFinal = line + "c";
+        if(automata.verificar(LineFinal)){
+            ui.print("Se cumple el patron necesario");
+        }else{
+            ui.print("No se cumple el patron necesario");
+        }
 
     }
 }
